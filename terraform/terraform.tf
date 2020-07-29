8587db7758804508ab12296f9b6684fe9719d6ed
“Craig <cmargieson@gmail.com>
2020-07-29 14:29:48 +1000

    add cluster security group

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+// Access load balancer from 80
 resource "aws_security_group" "react-template-lb-security-group" {
   egress {
     cidr_blocks = ["0.0.0.0/0"]
@@ -17,3 +18,21 @@ resource "aws_security_group" "react-template-lb-security-group" {
   }
   name = "react-template-lb-security-group"
 }
+
+// Access cluster from load balancer only
+resource "aws_security_group" "react-template-cluster-security-group" {
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+  ingress {
+    from_port       = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.react-template-lb-security-group.id]
+    to_port         = 80
+  }
+  name = "react-template-cluster-security-group"
+}
+
