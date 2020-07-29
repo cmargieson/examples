fd0a30dc51d6ab6e365341d9a202ee79ea88382d
“Craig <cmargieson@gmail.com>
2020-07-29 16:47:01 +1000

    Add load balancer and networking

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,20 @@
 variable "aws_region" {
   default = "ap-southeast-2"
 }
+
+variable "long_name" {
+  default = "React Template"
+}
+
+variable "short_name" {
+  default = "react-template"
+}
+
+variable "public_subnet_count" {
+  default = 2
+}
+
+variable "application_ecs_service_count" {
+  default = 3
+}
+
